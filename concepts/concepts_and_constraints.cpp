cc8324693773015208e5352fd597b1703b3cec06
Krystian Piękoś <krystian.piekos@infotraining.pl>
2023-12-13 15:05:33 +0000

    Concepts - 1

--- concepts/concepts_and_constraints.cpp
+++ concepts/concepts_and_constraints.cpp
@@ -1,27 +1,158 @@
+#include <cassert>
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
-#include <vector>
-#include <string>
 #include <map>
+#include <string>
+#include <vector>
 
 using namespace std::literals;
 
-template <typename TContainer>
-void print(const TContainer& c, std::string_view prefix = "items")
+namespace Traits
 {
-    std::cout << prefix << ": [ ";
-    for(const auto& item : c)
-        std::cout << item << " ";
-    std::cout << "]\n";
+    template <typename T>
+    struct is_void
+    {
+        static constexpr bool value = false;
+    };
+
+    template <>
+    struct is_void<void>
+    {
+        static constexpr bool value = true;
+    };
+
+    // template variable
+    template <typename T>
+    constexpr bool is_void_v = is_void<T>::value;
+
+    /////////////////////////////////////////////
+
+    template <typename T>
+    struct is_pointer
+    {
+        static constexpr bool value = false;
+    };
+
+    template <typename T>
+    struct is_pointer<T*>
+    {
+        static constexpr bool value = true;
+    };
+
+    // template variable
+    template <typename T>
+    constexpr bool is_pointer_v = is_pointer<T>::value;
+} // namespace Traits
+
+template <typename T>
+void foo()
+{
+    static_assert(!Traits::is_void_v<T>, "T cannot be void");
 }
 
-TEST_CASE("constraints")
+template <typename T>
+decltype(auto) deref(T ptr)
 {
-    print(std::vector{1, 2, 3}, "vec");
-    REQUIRE(true);
+    static_assert(Traits::is_pointer_v<T>, "T must a pointer type");
+    assert(ptr != nullptr);
+    return *ptr;
 }
 
-TEST_CASE("concepts")
+TEST_CASE("traits")
 {
-    REQUIRE(true);
+    foo<int>();
+    // foo<void>();
+
+    int x = 10;
+    CHECK(deref(&x) == 10);
+    // deref(x);
+
+    std::string str = "text";
+    deref(&str) = "abc";
+    CHECK(str == "abc"s);
+}
+
+////////////////////////////////////////////////////
+
+namespace ver_1
+{
+    template <typename T1, typename T2>
+    auto max_value(T1 a, T2 b)
+    {
+        return a < b ? b : a;
+    }
+
+    template <typename T>
+        requires Traits::is_pointer_v<T>
+    auto max_value(T a, auto b)
+        requires(Traits::is_pointer_v<decltype(b)>) // trailing requires clause
+    {
+        assert(a != nullptr);
+        assert(b != nullptr);
+        return *a < *b ? *b : *a;
+    }
+} // namespace ver_1
+
+template <typename T>
+concept Pointer = Traits::is_pointer_v<T>;
+
+namespace ver_2
+{
+    template <typename T1, typename T2>
+    auto max_value(T1 a, T2 b)
+    {
+        return a < b ? b : a;
+    }
+
+    template <typename T>
+        requires Pointer<T>
+    auto max_value(T a, auto b)
+        requires(Pointer<decltype(b)>) // trailing requires clause
+    {
+        assert(a != nullptr);
+        assert(b != nullptr);
+        return *a < *b ? *b : *a;
+    }
+} // namespace ver_2
+
+namespace ver_3
+{
+    template <typename T>
+    auto max_value(T a, T b)
+    {
+        return a < b ? b : a;
+    }
+
+    template <Pointer T>
+    auto max_value(T a, T b)
+    {
+        assert(a != nullptr);
+        assert(b != nullptr);
+        return *a < *b ? *b : *a;
+    }
+} // namespace ver_3
+
+auto max_value(auto a, auto b)
+{
+    return a < b ? b : a;
+}
+
+auto max_value(Pointer auto a, Pointer auto b)
+{
+    assert(a != nullptr);
+    assert(b != nullptr);
+    return *a < *b ? *b : *a;
+}
+
+TEST_CASE("max_value")
+{
+    int x = 10;
+    int y = 20;
+    CHECK(max_value(x, y) == 20);
+
+    CHECK(max_value(&x, &y) == 20);
+
+    static_assert(Pointer<int*>);
+    static_assert(Pointer<const double*>);
+    static_assert(!Pointer<int>);
 }
--- templates/templates.cpp
+++ templates/templates.cpp
@@ -1,11 +1,61 @@
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 
 using namespace std::literals;
 
-TEST_CASE("templates & lambda expressions")
+auto lambda_cpp14 = [](auto a, auto b) {
+    return a + b;
+};
+
+auto fun_cpp20(auto a, auto b)
 {
-    REQUIRE(true);
+    return a + b;
+}
+
+namespace CompilerInterpretetion
+{
+    template <typename T1, typename T2>
+    auto fun_cpp20(T1 a, T2 b)
+    {
+        return a + b;
+    }
+} // namespace CompilerInterpretetion
+
+void print_container(const auto& container, std::string_view name)
+{
+    std::cout << name << ": { ";
+    for(const auto& item : container)
+        std::cout << item << " ";
+    std::cout << "}\n";
+};
+
+TEST_CASE("functions with auto params")
+{
+    CHECK(fun_cpp20(42, 662u) == 704);
+    CHECK(fun_cpp20("abc"s, "def"s) == "abcdef"s);
+
+    std::vector vec = {1, 2, 3};
+    print_container(vec, "vec");
+}
+
+/////////////////////////////////////////////
+
+decltype(auto) call_wrapper(auto f, auto&&... args)
+{
+    std::cout << "Calling a function!\n";
+
+    return f(std::forward<decltype(args)>(args)...);
+}
+
+int foo(int x)
+{
+    return x * 2;
+}
+
+TEST_CASE("call wrapper")
+{
+    auto result = call_wrapper(foo, 42);
+    CHECK(result == 84);
 }
