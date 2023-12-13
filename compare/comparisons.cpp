ec3b32a88c0b17849d79066f2aac9447b01b91b6
Krystian Piękoś <krystian.piekos@infotraining.pl>
2023-12-13 10:22:09 +0000

    operator ==

--- compare/comparisons.cpp
+++ compare/comparisons.cpp
@@ -50,3 +50,41 @@ TEST_CASE("safe comparisons between integral numbers")
         CHECK(std::in_range<uint8_t>(257) == false);
     }
 }
+
+struct Point
+{
+    int x, y;
+
+    Point(int x = 0, int y = 0) : x{x}, y{y}
+    {}
+
+    bool operator==(const Point& other) const = default;
+};
+
+struct NamedPoint : Point
+{
+    std::string name;
+
+    NamedPoint(int x = 0, int y = 0, std::string name = "not-set") : Point{x, y}, name{std::move(name)}
+    {}
+
+    bool operator==(const NamedPoint& npt) const = default;
+};
+
+TEST_CASE("operator ==")
+{
+    Point pt{10, 20};
+
+    CHECK(pt == Point{10, 20});
+    CHECK(pt != Point{20, 10}); // rewriting of expression: !(pt == Point{20, 10})
+
+    Point pt_other = 30;   // Point(30, 0)
+    CHECK(pt_other == 30); // implicit conversion: pt_other == Point(30)
+    CHECK(30 == pt_other); // rewriting of expression: pt_other == 30 -> pt_other == Point(30)
+
+    NamedPoint named_pt{0, 0, "origin"};
+    CHECK(named_pt == NamedPoint{0, 0, "origin"});
+    CHECK(named_pt != NamedPoint{0, 0});
+
+    CHECK(named_pt == Point{0, 0}); // works with base class
+}
